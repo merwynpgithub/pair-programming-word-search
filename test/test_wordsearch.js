71866c10583a88e76704a3911863bcdb7f366932
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-19 10:30:51 -0500

    updated wordSearch for edge cases and added tests for it

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const {wordSearch, diagwordSearch} = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -32,6 +32,72 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([
+      [],
+      [],
+      [],
+      [],
+    ], 'AA');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the vertical word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'QFQEYN')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the reversed word is present horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LREYSCHW');
+
+    assert.isTrue(result);
+  });
+  it("should return true if the reversed word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'AAAYRRA');
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present diagonally", function() {
+    const result = diagwordSearch([
+      ['1', '7', '4'],
+      ['S', '3', '5'],
+      ['2', '6', '8']
+    ], 'WI');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,68 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
+        //check if the strings are empty
+        if (!l.length) return false;
         if (l.includes(word)) return true
     }
+    const verticalJoin = [];
+    for (let i = 0; i < letters[i].length; i++) {
+        let verticalWordString = "";
+        for (let j = 0; j < letters.length; j++) {
+            verticalWordString += letters[j][i];
+        }
+        verticalJoin.push(verticalWordString);
+    }
+    for (l of verticalJoin) {
+        //check if the strings are empty
+        if (!l.length) return false;
+        if (l.includes(word)) return true
+    }
+    const reverseJoin = [];
+    horizontalJoin.forEach(element => {
+        let word = element.split('').reverse().join('');
+        reverseJoin.push(word);
+    });
+    for (l of reverseJoin) {
+        //check if the strings are empty
+        if (!l.length) return false;
+        if (l.includes(word)) return true;
+    }
+    const reverseVerticalJoin = [];
+    verticalJoin.forEach(element => {
+        let word = element.split('').reverse().join('');
+        reverseVerticalJoin.push(word);
+    });
+    for (l of reverseVerticalJoin) {
+        //check if the strings are empty
+        if (!l.length) return false;
+        if (l.includes(word)) return true;
+    }
+
+    return false;
 }
 
-module.exports = wordSearch
+const diagwordSearch = (letters, word) => {
+    const diagonalJoin = [];
+    let diagWordStrLen = letters.length + letters[0].length;
+    let diaglen = letters[0].length;
+    let counter = 0;
+    for (let i = 0; i < diagWordStrLen; i++) {
+        let word = "";
+        for (let j = 0; j < diaglen; j++) {
+            word += letters[j][j + counter];
+            // counter++;
+        }
+        diagonalJoin.push(word);
+        console.log(diagonalJoin);
+        diaglen -= 1;
+        counter++;
+    }
+    // console.log(diagonalJoin);
+    return false;
+};
+
+
+module.exports = { wordSearch, diagwordSearch }
+
+//collab with github user- @merwynp and @abhiram-satha
